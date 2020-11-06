f7117c0e113f203c815fe425866442bd49f49942
denis <deniskoliban@gmail.com>
2020-11-06 18:01:08 +0200

    Set up content components to content container module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { NgModule } from '@angular/core';
-import { AppMaterialModule } from './modules/app.material.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ContainerComponentsModule} from './modules/container.components.module';
 
 import { AppComponent } from './app.component';
-import { ContentComponent } from './components/content/content.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContentContainerComponent } from './components/content-container/content-container.component';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContentComponent,
-    HeaderComponent,
-    ContentContainerComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppMaterialModule,
-    BrowserAnimationsModule
+    ContainerComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
