231a9394a16a5f44f2efbc78411f1a5d6dff954b
denis <deniskoliban@gmail.com>
2020-11-16 12:24:56 +0200

    Get user data from DB by logging in

--- src/app/content-components/content/content.component.ts
+++ src/app/content-components/content/content.component.ts
@@ -4,6 +4,8 @@ import {SidenavService} from '../../services/services/sidenav.service';
 import {UserService} from '../../services/services/user.service';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../store/app.reducer';
+import {HttpClient} from '@angular/common/http';
+import {map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-content',
@@ -17,11 +19,28 @@ export class ContentComponent implements OnInit, AfterViewInit {
   constructor(
     private sidenavService: SidenavService,
     private store: Store<AppState>,
-    private userService: UserService
+    private userService: UserService,
+    private http: HttpClient
     ) { }
 
   ngOnInit(): void {
-    this.store.select('auth').subscribe((state) => {
+    this.store.select('auth').pipe(take(1)).subscribe((state) => {
+        if (state.user) {
+          this.http.get(
+            `https://mysocialnetwork-ee2a9.firebaseio.com/users.json`
+          ).pipe(
+            map( (response) => {
+              return response[state.user.localId];
+            })
+          ).subscribe(
+            (responce) => {
+              this.userData = responce;
+              console.log(responce);
+            }, error => {
+              console.log(error);
+            }
+          );
+        }
     });
   }
 
