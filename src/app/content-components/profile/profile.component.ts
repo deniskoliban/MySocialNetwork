b987fd7ac39093267e47b6f0e5a20c1f138c88ce
denis <deniskoliban@gmail.com>
2020-11-20 17:50:59 +0200

    Add profile uploading photo

--- src/app/content-components/profile/profile.component.ts
+++ src/app/content-components/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {UserData} from '../../components/auth/store/authReducer';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../store/app.reducer';
+import {AngularFireStorage} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-profile',
@@ -9,9 +10,11 @@ import {AppState} from '../../store/app.reducer';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('file', {static: true}) file: ElementRef;
+  @ViewChild('avatar', {static: true}) avatar: ElementRef;
   userData: UserData;
 
-  constructor(private store: Store<AppState>,) { }
+  constructor(private store: Store<AppState>, private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
     this.store.select('auth').subscribe(
@@ -19,6 +22,24 @@ export class ProfileComponent implements OnInit {
         this.userData = state.userData;
       }
     );
+  }
+
+  upload(): void {
+    const ref =  this.storage.storage.ref();
+    const img = this.file.nativeElement.files[0];
+    const name = img.name;
+    const metadata = {
+      contentType: img.type
+    };
+    const task = ref.child(name).put(img, metadata);
+
+    task.then(
+      snapshot => snapshot.ref.getDownloadURL()
+    ).then(ur => this.avatar.nativeElement.style.backgroundImage = `url(${ur})`);
+  }
+
+  chooseFile(): void {
+    this.file.nativeElement.click();
   }
 
 }
