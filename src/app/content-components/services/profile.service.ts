c57a9b95eaaaf80cfe7bb5947079fa440e381f1c
denis <deniskoliban@gmail.com>
2020-11-23 10:57:57 +0200

    Create getProfile method for fetching profile

--- src/app/content-components/services/profile.service.ts
+++ src/app/content-components/services/profile.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../store/app.reducer';
+import {Observable} from 'rxjs';
+import {UserData} from '../../components/auth/store/authReducer';
+import {Profile} from '../profile/store/profile.reducer';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpService {
+export class ProfileService {
+  localId: string = null;
 
-  constructor() { }
+  constructor(private http: HttpClient, private store: Store<AppState> ) {
+    this.store.select('auth').subscribe((state) => {
+      this.localId = state.user.localId;
+    });
+  }
+
+  getProfile(): Observable<Profile> {
+    return this.http.get<Profile>(
+      `https://mysocialnetwork-ee2a9.firebaseio.com/users/${this.localId}/profile.json`
+    );
+  }
+
 }
