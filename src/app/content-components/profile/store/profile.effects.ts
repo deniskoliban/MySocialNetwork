6561c8fa04691d1dad2085fdbe395039c45acd7c
denis <deniskoliban@gmail.com>
2020-11-23 10:58:23 +0200

    Create  getProfileEffect

--- src/app/content-components/profile/store/profile.effects.ts
+++ src/app/content-components/profile/store/profile.effects.ts
@@ -1,9 +1,24 @@
 import {Injectable} from '@angular/core';
-
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import * as fromProfileActions from './profile.actions';
+import {map, switchMap} from 'rxjs/operators';
+import {ProfileService} from '../../services/profile.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MessagesEffects {
+export class ProfileEffects {
+
+  getProfileEffect$ = createEffect(() => this.actions$.pipe(
+    ofType(fromProfileActions.getProfile),
+    switchMap(() => {
+      return this.profileService.getProfile().pipe(
+        map((profile) => fromProfileActions.getProfileSuccess({profile}))
+      );
+    })
+  ));
+
+  constructor(private actions$: Actions, private profileService: ProfileService) {
+  }
 
 }
