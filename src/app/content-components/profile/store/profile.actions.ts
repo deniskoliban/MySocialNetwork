218fe032d69cfdc3cdc958f884a484e71eee7c85
denis <deniskoliban@gmail.com>
2020-11-23 11:21:20 +0200

    Create 'PUT_PROFILE' & 'PUT_PROFILE_SUCCESS' actions

--- src/app/content-components/profile/store/profile.actions.ts
+++ src/app/content-components/profile/store/profile.actions.ts
@@ -3,10 +3,18 @@ import {Profile} from './profile.reducer';
 
 export const getProfile = createAction(
   'GET_PROFILE',
-  props<{localId: string}>()
 );
 
 export const getProfileSuccess = createAction(
   'GET_PROFILE_SUCCESS',
   props<{profile: Profile}>()
+);
+
+export const putProfile = createAction(
+  'PUT_PROFILE',
+  props<{profile: Profile}>()
+);
+
+export const putProfileSuccess = createAction(
+  'PUT_PROFILE_SUCCESS',
 );
