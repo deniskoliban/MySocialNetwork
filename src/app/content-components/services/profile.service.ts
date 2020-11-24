e5682695b4fb58a66e5e4f4fd28691f58a35e97a
denis <deniskoliban@gmail.com>
2020-11-24 11:04:02 +0200

    Create uploadAvatar method in ProfileService to upload avatara url to firebase

--- src/app/content-components/services/profile.service.ts
+++ src/app/content-components/services/profile.service.ts
@@ -3,35 +3,42 @@ import {HttpClient} from '@angular/common/http';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../store/app.reducer';
 import {Observable} from 'rxjs';
-import {Profile} from '../profile/store/profile.reducer';
-import {State} from '../../components/auth/store/authReducer';
+import * as ProfileState from '../profile/store/profile.reducer';
+import * as AuthState from '../../components/auth/store/authReducer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  state: State;
+  authState: AuthState.State;
 
   constructor(private http: HttpClient, private store: Store<AppState> ) {
     this.store.select('auth').subscribe((state) => {
-      this.state = state;
-      console.log(this.state);
+      this.authState = state;
+      console.log(this.authState);
     });
     this.store.select('profile').subscribe((state) => {
       console.log(state);
     });
   }
 
-  putProfile(profile: Profile): Observable<Profile> {
-    return this.http.put<Profile>(
-      `https://mysocialnetwork-ee2a9.firebaseio.com/users/${this.state.user.localId}/profile.json`,
+  uploadAvatar(url: string): Observable<string> {
+    return this.http.put<string>(
+      `https://mysocialnetwork-ee2a9.firebaseio.com/users/${this.authState.user.localId}/avatarUrl.json`,
+      {url}
+    );
+  }
+
+  putProfile(profile: ProfileState.Profile): Observable<ProfileState.Profile> {
+    return this.http.put<ProfileState.Profile>(
+      `https://mysocialnetwork-ee2a9.firebaseio.com/users/${this.authState.user.localId}/profile.json`,
       {...profile}
     );
   }
 
-  getProfile(): Observable<Profile> {
-    return this.http.get<Profile>(
-      `https://mysocialnetwork-ee2a9.firebaseio.com/users/${this.state.user.localId}/profile.json`
+  getProfile(): Observable<{ profile: ProfileState.Profile, avatarUrl: {url: string} }> {
+    return this.http.get<{ profile: ProfileState.Profile, avatarUrl: {url: string} }>(
+      `https://mysocialnetwork-ee2a9.firebaseio.com/users/${this.authState.user.localId}.json`
     );
   }
 
